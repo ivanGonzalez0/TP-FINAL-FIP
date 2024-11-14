cbfc465460bc0a1508e21dedb0a925416147080e
ivanGonzalez0 <ig011100011@gmail.com>
2024-11-14 18:47:04 -0300

    cree la clase veterinaria

--- veterinaria.ts
+++ veterinaria.ts
@@ -1,15 +1,103 @@
-class Veterinaria {
+export interface Veterinaria {
+    numeroId(): void;
+    altaPaciente(): void;
+    bajaPaciente(): void;
+    modificarDatos(): void;
+}
+// Veterinaria 1
+export class Veterinaria1 implements Veterinaria {
     private nombre: string;
     private direccion: string;
     private id: number;
 
-    constructor(nombre: string, direccion: string, id: number) {
+    public constructor(nombre: string, direccion: string, id: number) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.id = id;
     }
-    // metodo que genera un numero aleatorio para el id
-    numeroAleatorio(min: number, max: number): number {
-        return Math.floor(Math.random() * (min - max)) + min;
+
+    // getters
+    public getNombre(): string {
+        return this.nombre;
+    }
+    public getDireccion(): string {
+        return this.direccion;
+    }
+    public getNumeroId(): number {
+        return this.id;
+    }
+    // setters
+    public setNombre(): void {
+        this.nombre = this.nombre;
+    }
+    public setDireccion(): void {
+        this.direccion = this.direccion;
+    }
+    public setNumeroId(): void {
+        this.id = this.id;
+    }
+
+    //Metodos
+    public altaPaciente(): void {
+        console.log("Alta paciente");
+    }
+    public bajaPaciente(): void {
+        console.log("Baja paciente");
+    }
+    public modificarDatos(): void {
+        console.log("Modificacion de datos");
+    }
+    public numeroId(): void {
+        console.log(Math.random());
+    }
+}
+// Veterinaria 2
+export class Veterinaria2 implements Veterinaria {
+    private nombre: string;
+    private direccion: string;
+    private id: number;
+
+    public constructor(nombre: string, direccion: string, id: number) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.id = id;
+    }
+    
+    public altaPaciente(): void {
+        console.log("Alta paciente");
+    }
+    public bajaPaciente(): void {
+        console.log("Baja paciente");
+    }
+    public modificarDatos(): void {
+        console.log("Modificacion de datos");
+    }
+    public numeroId(): void {
+        console.log(Math.random());
+    }
+}
+// Veterinaria 3
+export class Veterinaria3 implements Veterinaria{
+    private nombre: string;
+    private direccion: string;
+    private id: number;
+
+    public constructor(nombre: string, direccion: string, id: number) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.id = id;
+    }
+    
+    public altaPaciente(): void {
+        console.log("Alta paciente");
+    }
+    public bajaPaciente(): void {
+        console.log("Baja paciente");
+    }
+    public modificarDatos(): void {
+        console.log("Modificacion de datos");
+    }
+    public numeroId(): void {
+        console.log(Math.random());
     }
 }
